5eac13f0f3a3db304b6f55fbfda103a3ae775724
Shrey Banga <banga.shrey@gmail.com>
2022-08-22 23:08:48 -0400

    Implement Glass material
    This highlighted several bugs which were caught and fixed by adding asserts
    and tests in previous and current commits.

--- src/material.rs
+++ src/material.rs
@@ -1,6 +1,42 @@
+use approx::{assert_abs_diff_eq};
+use rand::Rng;
+
 use crate::{
-    color::Color, intersection::Intersection, ray::Ray, sampling::sample_hemisphere, trace, Scene,
+    color::Color, intersection::Intersection, ray::Ray, sampling::sample_hemisphere, trace,
+    vector::Vector, Scene, constants::EPSILON
 };
+
+fn reflect(direction: &Vector, normal: &Vector) -> Vector {
+    *direction - *normal * (normal.dot(direction) * 2.0)
+}
+
+fn refract(direction: &Vector, normal: &Vector, eta_i: f64, eta_t: f64) -> Vector {
+    let mut normal = *normal;
+    let mut cos_theta = -direction.dot(&normal);
+    let mut eta_relative = eta_t / eta_i;
+    if cos_theta < 0.0 {
+        cos_theta = -cos_theta;
+        eta_relative = eta_i / eta_t;
+        normal = normal * -1.0;
+    }
+
+    let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
+    if sin_theta > eta_relative {
+        return reflect(direction, &normal);
+    }
+
+    // Schlick approximation of Fresnel reflectance
+    let mut rng = rand::thread_rng();
+    let r0 = ((1.0 - eta_relative) / (1.0 + eta_relative)).powf(2.0);
+    let reflectance = r0 + (1.0 - r0) * (1.0 - cos_theta).powf(5.0);
+    if rng.gen::<f64>() < reflectance {
+        return reflect(direction, &normal);
+    }
+
+    let r_perpendicular = (*direction + normal * cos_theta) / eta_relative;
+    let r_parallel = normal * -(1.0 - r_perpendicular.dot(&r_perpendicular)).sqrt();
+    r_perpendicular + r_parallel
+}
 
 pub trait Material: Sync + Send {
     fn sample(&self, scene: &Scene, intersection: &Intersection, ray: &Ray, depth: u32) -> Color;
@@ -33,11 +69,52 @@ pub struct Mirror {
 
 impl Material for Mirror {
     fn sample(&self, scene: &Scene, intersection: &Intersection, ray: &Ray, depth: u32) -> Color {
-        let ray = Ray::new(
-            intersection.location,
-            intersection.normal
-                - intersection.normal * (intersection.normal.dot(&ray.direction) * 2.0),
-        );
+        let direction = reflect(&ray.direction, &intersection.normal);
+        assert_abs_diff_eq!(direction.magnitude(), 1.0, epsilon=EPSILON);
+
+        let ray = Ray {
+            origin: intersection.location,
+            direction,
+        };
         trace(&ray, scene, depth) * self.reflectance
+    }
+}
+
+pub struct Glass {
+    pub eta: f64,
+}
+
+impl Material for Glass {
+    fn sample(&self, scene: &Scene, intersection: &Intersection, ray: &Ray, depth: u32) -> Color {
+        let direction = refract(&ray.direction, &intersection.normal, 1.0, self.eta);
+        assert_abs_diff_eq!(direction.magnitude(), 1.0, epsilon=EPSILON);
+    
+        let ray = Ray {
+            origin: intersection.location,
+            direction,
+        };
+        return trace(&ray, scene, depth);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::vector::*;
+
+    #[test]
+    fn reflect_test() {
+        assert_abs_diff_eq!(
+            reflect(&Vector(1.0, -1.0, 0.0).normalized(), &Vector(0.0, 1.0, 0.0)),
+            Vector(1.0, 1.0, 0.0).normalized()
+        );
+    }
+
+    #[test]
+    fn refract_test() {
+        assert_abs_diff_eq!(
+            refract(&Vector(1.0, -1.0, 0.0).normalized(), &Vector(0.0, 1.0, 0.0), 1.0, 1.0),
+            Vector(1.0, -1.0, 0.0).normalized()
+        );
     }
 }
--- src/scenes.rs
+++ src/scenes.rs
@@ -5,7 +5,7 @@ use rand::Rng;
 use crate::{
     camera::ProjectionCamera,
     color::Color,
-    material::{LambertianMaterial, Material, Mirror},
+    material::{LambertianMaterial, Material, Mirror, Glass},
     scene::Scene,
     shape::{Shape, Sphere},
     vector::Vector,
@@ -13,15 +13,14 @@ use crate::{
 
 #[allow(dead_code)]
 pub fn simple() -> Scene {
-    let num_samples: usize = 4;
-    let num_camera_samples: usize = 256;
+    let num_camera_samples: usize = 1024;
     let film_width: usize = 800;
-    let film_height: usize = 470;
+    let film_height: usize = 480;
 
     let blue = Color::from_rgb(0, 120, 255);
 
     Scene {
-        max_depth: 3,
+        max_depth: 4,
         gamma: 2.2,
         film_width,
         film_height,
@@ -37,18 +36,18 @@ pub fn simple() -> Scene {
         )),
         shapes: vec![
             Box::new(Sphere {
-                origin: Vector(-1.0, 1.0, 10.0),
+                origin: Vector(0.0, 1.0, 12.5),
                 radius: 1.0,
                 material: Box::new(LambertianMaterial {
                     reflectance: blue,
-                    num_samples,
+                    num_samples: 1,
                 }),
             }),
             Box::new(Sphere {
                 origin: Vector(0.5, 0.5, 10.5),
                 radius: 0.5,
-                material: Box::new(Mirror {
-                    reflectance: Color::WHITE,
+                material: Box::new(Glass {
+                    eta: 1.3,
                 }),
             }),
             Box::new(Sphere {
@@ -56,7 +55,7 @@ pub fn simple() -> Scene {
                 radius: 1000.0,
                 material: Box::new(LambertianMaterial {
                     reflectance: Color::WHITE,
-                    num_samples,
+                    num_samples: 1,
                 }),
             }),
         ],
