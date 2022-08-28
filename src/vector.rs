b8241cca2ba77954873aee00c3b8f4f858ceb9b1
Shrey Banga <banga.shrey@gmail.com>
2022-08-28 18:59:59 -0400

    Stop using VectorIterator
    This made rendering 5x faster!

--- src/vector.rs
+++ src/vector.rs
@@ -1,6 +1,5 @@
 use std::{
     fmt::Display,
-    iter::FromIterator,
     ops::{Add, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign},
 };
 
@@ -17,9 +16,6 @@ impl Vector {
     pub const Y: Vector = Vector(0.0, 1.0, 0.0);
     pub const Z: Vector = Vector(0.0, 0.0, 1.0);
     pub const NULL: Vector = Vector(0.0, 0.0, 0.0);
-    pub fn iter(&self) -> VectorIterator {
-        VectorIterator::new(&self)
-    }
     pub fn x(&self) -> f64 {
         self.0
     }
@@ -30,14 +26,14 @@ impl Vector {
         self.2
     }
     pub fn magnitude(&self) -> f64 {
-        self.iter().map(|x| x * x).sum::<f64>().sqrt()
+        (self.0 * self.0 + self.1 * self.1 + self.2 * self.2).sqrt()
     }
     pub fn normalized(&self) -> Vector {
         let mag = self.magnitude();
         Vector(self.x() / mag, self.y() / mag, self.z() / mag)
     }
     pub fn dot(&self, other: &Vector) -> f64 {
-        self.iter().zip(other.iter()).map(|(a, b)| a * b).sum()
+        self.0 * other.0 + self.1 * other.1 + self.2 * other.2
     }
     pub fn cross(&self, other: &Vector) -> Vector {
         Vector(
@@ -51,20 +47,9 @@ impl Vector {
     }
 }
 
-impl FromIterator<f64> for Vector {
-    fn from_iter<T: IntoIterator<Item = f64>>(iter: T) -> Self {
-        let mut i = iter.into_iter();
-        Vector(
-            i.next().unwrap_or(0.0),
-            i.next().unwrap_or(0.0),
-            i.next().unwrap_or(0.0),
-        )
-    }
-}
-
 impl PartialEq for Vector {
     fn eq(&self, other: &Self) -> bool {
-        self.iter().zip(other.iter()).all(|(x, y)| x == y)
+        self.0 == other.0 && self.1 == other.1 && self.2 == other.2
     }
 }
 
@@ -72,7 +57,7 @@ impl Add for Vector {
     type Output = Vector;
 
     fn add(self, rhs: Self) -> Self::Output {
-        self.iter().zip(rhs.iter()).map(|(x, y)| x + y).collect()
+        Vector(self.0 + rhs.0, self.1 + rhs.1, self.2 + rhs.2)
     }
 }
 
@@ -88,7 +73,7 @@ impl Sub for Vector {
     type Output = Vector;
 
     fn sub(self, rhs: Self) -> Self::Output {
-        self.iter().zip(rhs.iter()).map(|(x, y)| x - y).collect()
+        Vector(self.0 - rhs.0, self.1 - rhs.1, self.2 - rhs.2)
     }
 }
 
@@ -103,7 +88,7 @@ impl SubAssign for Vector {
 impl Mul<f64> for Vector {
     type Output = Vector;
     fn mul(self, rhs: f64) -> Self::Output {
-        Vector(self.x() * rhs, self.y() * rhs, self.z() * rhs)
+        Vector(self.0 * rhs, self.1 * rhs, self.2 * rhs)
     }
 }
 
@@ -118,7 +103,7 @@ impl MulAssign<f64> for Vector {
 impl Div<f64> for Vector {
     type Output = Vector;
     fn div(self, rhs: f64) -> Self::Output {
-        Vector(self.x() / rhs, self.y() / rhs, self.z() / rhs)
+        Vector(self.0 / rhs, self.1 / rhs, self.2 / rhs)
     }
 }
 
@@ -127,14 +112,6 @@ impl DivAssign<f64> for Vector {
         self.0 /= rhs;
         self.1 /= rhs;
         self.2 /= rhs;
-    }
-}
-
-// Allow pairwise multiplication of two vectors
-impl Mul<Vector> for Vector {
-    type Output = Vector;
-    fn mul(self, rhs: Vector) -> Self::Output {
-        Vector(self.x() * rhs.x(), self.y() * rhs.y(), self.z() * rhs.z())
     }
 }
 
@@ -155,28 +132,6 @@ impl AbsDiffEq for Vector {
 impl Display for Vector {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "({},{},{})", self.0, self.1, self.z())
-    }
-}
-
-pub struct VectorIterator<'a>(&'a Vector, i8);
-
-impl VectorIterator<'_> {
-    fn new(v: &Vector) -> VectorIterator {
-        VectorIterator(v, -1)
-    }
-}
-
-impl Iterator for VectorIterator<'_> {
-    type Item = f64;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        self.1 += 1;
-        match self.1 {
-            0 => Some(self.0 .0),
-            1 => Some(self.0 .1),
-            2 => Some(self.0 .2),
-            _ => None,
-        }
     }
 }
 
