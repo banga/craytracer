6466bb09dfa0599aae9ae550c371dbb52d8766f3
Shrey Banga <banga.shrey@gmail.com>
2022-08-28 22:29:25 -0400

    Add triangle shape

--- src/shape.rs
+++ src/shape.rs
@@ -46,48 +46,201 @@ impl Shape for Sphere {
     }
 }
 
+pub struct Triangle {
+    v0: Vector,
+    e1: Vector,
+    e2: Vector,
+    normal: Vector,
+    material: Box<dyn Material>,
+}
+
+impl Triangle {
+    pub fn new(v0: Vector, v1: Vector, v2: Vector, material: Box<dyn Material>) -> Triangle {
+        let e1 = v1 - v0;
+        let e2 = v2 - v0;
+        Triangle {
+            v0,
+            e1,
+            e2,
+            normal: e2.cross(&e1).normalized(),
+            material,
+        }
+    }
+}
+
+impl Shape for Triangle {
+    #[allow(non_snake_case)]
+    fn intersect(&self, ray: &Ray) -> Option<Intersection> {
+        // Source: http://www.graphics.cornell.edu/pubs/1997/MT97.pdf
+        let P = ray.direction.cross(&self.e2);
+
+        let denominator = P.dot(&self.e1);
+        if denominator > -EPSILON && denominator < EPSILON {
+            return None;
+        }
+
+        let T = ray.origin - self.v0;
+        let inv_denominator = 1.0 / denominator;
+        let u = P.dot(&T) * inv_denominator;
+        if u < 0.0 || u > 1.0 {
+            return None;
+        }
+
+        let Q = T.cross(&self.e1);
+        let v = Q.dot(&ray.direction) * inv_denominator;
+        if v < 0.0 || u + v > 1.0 {
+            return None;
+        }
+
+        let distance = T.cross(&self.e1).dot(&self.e2) * inv_denominator;
+        if distance < EPSILON {
+            return None;
+        }
+
+        let location = ray.at(distance);
+
+        Some(Intersection {
+            distance,
+            location,
+            normal: self.normal,
+            shape: self,
+        })
+    }
+
+    fn material(&self) -> &Box<dyn Material> {
+        &self.material
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::{color::Color, material::LambertianMaterial};
 
-    #[test]
-    fn intersect() {
-        // Unit sphere at origin
-        let sphere = Sphere {
-            origin: Vector(0.0, 0.0, 0.0),
-            radius: 1.0,
-            material: Box::new(LambertianMaterial {
-                reflectance: Color::WHITE,
-            }),
-        };
+    mod sphere {
+        use super::*;
 
-        // Shoot ray from outside sphere
-        let intersection = sphere
-            .intersect(&Ray {
-                origin: Vector(0.0, 0.0, -2.0),
-                direction: Vector::Z,
-            })
-            .unwrap();
-        assert_eq!(intersection.location, Vector(0.0, 0.0, -1.0));
-        assert_eq!(intersection.normal, Vector(0.0, 0.0, -1.0));
-
-        // Shoot ray from inside sphere
-        let intersection = sphere
-            .intersect(&Ray {
+        #[test]
+        fn intersect() {
+            // Unit sphere at origin
+            let sphere = Sphere {
                 origin: Vector(0.0, 0.0, 0.0),
-                direction: Vector::Z,
-            })
-            .unwrap();
-        assert_eq!(intersection.location, Vector(0.0, 0.0, 1.0));
-        assert_eq!(intersection.normal, Vector(0.0, 0.0, 1.0));
+                radius: 1.0,
+                material: Box::new(LambertianMaterial {
+                    reflectance: Color::WHITE,
+                }),
+            };
 
-        // Shoot ray away from sphere
-        assert!(sphere
-            .intersect(&Ray {
-                origin: Vector(0.0, 0.0, -2.0),
-                direction: Vector::X,
-            })
-            .is_none());
+            // Shoot ray from outside sphere
+            let intersection = sphere
+                .intersect(&Ray {
+                    origin: Vector(0.0, 0.0, -2.0),
+                    direction: Vector::Z,
+                })
+                .unwrap();
+            assert_eq!(intersection.location, Vector(0.0, 0.0, -1.0));
+            assert_eq!(intersection.normal, Vector(0.0, 0.0, -1.0));
+
+            // Shoot ray from inside sphere
+            let intersection = sphere
+                .intersect(&Ray {
+                    origin: Vector(0.0, 0.0, 0.0),
+                    direction: Vector::Z,
+                })
+                .unwrap();
+            assert_eq!(intersection.location, Vector(0.0, 0.0, 1.0));
+            assert_eq!(intersection.normal, Vector(0.0, 0.0, 1.0));
+
+            // Shoot ray away from sphere
+            assert!(sphere
+                .intersect(&Ray {
+                    origin: Vector(0.0, 0.0, -2.0),
+                    direction: Vector::X,
+                })
+                .is_none());
+        }
+    }
+
+    mod triangle {
+        use approx::assert_abs_diff_eq;
+        use rand::{thread_rng, Rng};
+
+        use super::*;
+
+        // Triangle in XY plane
+        fn triangle() -> Triangle {
+            Triangle::new(
+                Vector(1.0, 0.0, 0.0),
+                Vector(1.0, 1.0, 0.0),
+                Vector(2.0, 0.0, 0.0),
+                Box::new(LambertianMaterial {
+                    reflectance: Color::WHITE,
+                }),
+            )
+        }
+
+        #[test]
+        fn intersect_vertices() {
+            // Shoot ray to hit v0
+            let t = triangle();
+            for point in [t.v0, t.v0 + t.e1, t.v0 + t.e2] {
+                let intersection = t
+                    .intersect(&Ray {
+                        origin: Vector(point.x(), point.y(), -2.0),
+                        direction: Vector::Z,
+                    })
+                    .unwrap();
+                assert_eq!(intersection.location, point);
+                assert_eq!(intersection.normal, Vector(0.0, 0.0, 1.0));
+            }
+        }
+
+        #[test]
+        fn from_behind() {
+            // Shoot ray from the opposite side
+            let t = triangle();
+            let intersection = t
+                .intersect(&Ray {
+                    origin: Vector(1.0, 0.0, 2.0),
+                    direction: Vector::Z * -1.0,
+                })
+                .unwrap();
+            assert_eq!(intersection.location, Vector(1.0, 0.0, 0.0));
+            assert_eq!(intersection.normal, Vector(0.0, 0.0, 1.0));
+        }
+
+        #[test]
+        fn parallel_to_triangle() {
+            assert!(triangle()
+                .intersect(&Ray {
+                    origin: Vector(0.0, 0.0, 0.0),
+                    direction: Vector(1.0, 1.0, 0.0).normalized(),
+                })
+                .is_none());
+        }
+
+        #[test]
+        fn random_point() {
+            let t = triangle();
+
+            let mut rng = thread_rng();
+            let u = rng.gen_range(0.0..1.0);
+            let v = rng.gen_range(0.0..1.0);
+            let target = t.v0 + t.e1 * u + t.e2 * v;
+
+            let origin = Vector(0.0, 0.0, -2.0);
+            let intersection = t.intersect(&Ray {
+                origin,
+                direction: (target - origin).normalized(),
+            });
+
+            if u + v <= 1.0 {
+                let intersection = intersection.expect("Expected an intersection");
+                assert_abs_diff_eq!(intersection.location, target, epsilon = EPSILON);
+                assert_eq!(intersection.normal, Vector(0.0, 0.0, 1.0));
+            } else {
+                assert!(intersection.is_none());
+            }
+        }
     }
 }
