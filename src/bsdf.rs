ea429d863047930868c3671d06658ce29100d2bf
Shrey Banga <banga.shrey@gmail.com>
2024-01-29 00:31:39 -0500

    Fix how BSDF handles reflection/transmission
    The is_reflection check in sample was totally incorrect, since normals can face in arbitrary directions depending on geometry. `sample` should really sample all bxdfs, then based on the sampled direction, we can figure out whether we should be doing a transmission or reflection.
    
    This fixes black meshes in the staircase scene.

--- src/bsdf.rs
+++ src/bsdf.rs
@@ -12,10 +12,48 @@ pub struct BSDF {
 }
 
 impl BSDF {
-    // TODO: This isn't quite right. The caller should be able to specify which
-    // bxdf types to use.
-    fn get_relevant_bxdfs(&self, w_o: &Vector, normal: &Normal) -> Vec<&BxDF> {
-        let is_reflecting = w_o.dot(normal) > 0.0;
+    pub fn sample(
+        &self,
+        (sample1, sample2): (Sample1d, Sample2d),
+        w_o: &Vector,
+        normal: &Normal,
+    ) -> Option<SurfaceSample> {
+        if self.bxdfs.len() == 0 {
+            return None;
+        }
+
+        let sample_index = (sample1.take() * self.bxdfs.len() as f64) as usize;
+        let bxdf = &self.bxdfs[sample_index];
+
+        let sample = bxdf.sample(sample2, w_o, normal);
+        if sample.is_none() {
+            return None;
+        }
+
+        let sample = sample.unwrap();
+        if let Pdf::NonDelta(mut pdf) = sample.pdf {
+            let mut f = sample.f;
+            for other_bxdf in self.get_relevant_bxdfs(w_o, &sample.w_i, normal).iter() {
+                if *other_bxdf != bxdf {
+                    f += other_bxdf.f(w_o, &sample.w_i, &normal);
+                    if let Pdf::NonDelta(other_pdf) = other_bxdf.pdf(w_o, &sample.w_i, normal) {
+                        pdf += other_pdf;
+                    }
+                }
+            }
+            Some(SurfaceSample {
+                w_i: sample.w_i,
+                pdf: Pdf::NonDelta(pdf / self.bxdfs.len() as f64),
+                f,
+                is_specular: sample.is_specular,
+            })
+        } else {
+            Some(sample)
+        }
+    }
+
+    fn get_relevant_bxdfs(&self, w_o: &Vector, w_i: &Vector, normal: &Normal) -> Vec<&BxDF> {
+        let is_reflecting = w_o.dot(normal) * w_i.dot(normal) > 0.0;
         self.bxdfs
             .iter()
             .filter(|b| {
@@ -28,50 +66,9 @@ impl BSDF {
             .collect()
     }
 
-    pub fn sample(
-        &self,
-        (sample1, sample2): (Sample1d, Sample2d),
-        w_o: &Vector,
-        normal: &Normal,
-    ) -> Option<SurfaceSample> {
-        let relevant_bxdfs = self.get_relevant_bxdfs(w_o, normal);
-        if relevant_bxdfs.len() == 0 {
-            return None;
-        }
-
-        let sample_index = (sample1.take() * relevant_bxdfs.len() as f64) as usize;
-        let bxdf = &relevant_bxdfs[sample_index];
-
-        let sample = bxdf.sample(sample2, w_o, normal);
-        if sample.is_none() {
-            return None;
-        }
-
-        let sample = sample.unwrap();
-        if let Pdf::NonDelta(mut pdf) = sample.pdf {
-            let mut f = sample.f;
-            for (index, other_bxdf) in relevant_bxdfs.iter().enumerate() {
-                if index != sample_index {
-                    f += other_bxdf.f(w_o, &sample.w_i, &normal);
-                    if let Pdf::NonDelta(other_pdf) = other_bxdf.pdf(w_o, &sample.w_i, normal) {
-                        pdf += other_pdf;
-                    }
-                }
-            }
-            Some(SurfaceSample {
-                w_i: sample.w_i,
-                pdf: Pdf::NonDelta(pdf / relevant_bxdfs.len() as f64),
-                f,
-                is_specular: sample.is_specular,
-            })
-        } else {
-            Some(sample)
-        }
-    }
-
     pub fn f(&self, w_o: &Vector, w_i: &Vector, normal: &Normal) -> Color {
         let mut f = Color::BLACK;
-        for bxdf in self.get_relevant_bxdfs(w_o, normal) {
+        for bxdf in self.get_relevant_bxdfs(w_o, w_i, normal) {
             f += bxdf.f(w_o, w_i, normal);
         }
         f
@@ -80,7 +77,7 @@ impl BSDF {
     pub fn pdf(&self, w_o: &Vector, w_i: &Vector, normal: &Normal) -> Pdf {
         let mut pdf = 0.0;
         let mut num_matching_bxdfs = 0;
-        for bxdf in self.get_relevant_bxdfs(w_o, normal) {
+        for bxdf in self.get_relevant_bxdfs(w_o, w_i, normal) {
             match bxdf.pdf(w_o, w_i, normal) {
                 Pdf::NonDelta(p) => {
                     pdf += p;
